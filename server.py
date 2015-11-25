03395654c3990e3feb859647da67595169bfc59e
Dominic May <jack.thatch@gmail.com>
2015-11-25 12:37:29 +0800

    Cleanup old code

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import re
 import os
-import time
-import json
 import logging
 from datetime import datetime
 
@@ -28,10 +26,6 @@ gmaps = googlemaps.Client(key=AUTH['GOOGLE_MAPS_DIRECTIONS'])
 
 
 MY_ADDRESS = 'http://ms.mause.me'
-CALLERS = json.load(open('callers.json'))
-for caller in CALLERS.values():
-    assert set(caller) == {'name', 'passcode', 'url'}
-    assert isinstance(caller['url'], (list, tuple))
 
 
 def url(url, params):
@@ -48,88 +42,6 @@ class Response(TwimlResponse):
 
 def make_res(res):
     return flask.Response(res.toxml(), mimetype='text/xml')
-
-
-@app.route('/call', methods=['POST'])
-def call():
-    return message_system(flask.request.form['To'])
-
-
-def message_system(number):
-    res = Response()
-    if number not in CALLERS:
-        res.say(
-            'Hello, unknown caller. '
-            'You are not authorized to access this service. '
-            'Goodbye'
-        )
-
-    else:
-        res.say('Hello, {}'.format(CALLERS[number]['name']))
-        action = url(
-            urljoin(MY_ADDRESS, '/gather_action'),
-            params={'number': number}
-        )
-
-        with res.gather(numDigits='12', action=action) as g:
-            g.say(
-                'Please enter your passcode, followed by the hash key',
-                language='en-AU'
-            )
-        res.say("I didn't catch that. Goodbye!")
-
-    return make_res(res)
-
-
-@app.route('/gather_action', methods=['GET', 'POST'])
-def gather_action():
-    digits = flask.request.form.get('Digits', '')
-    logging.info('Digits: %s', digits)
-
-    caller = CALLERS[flask.request.args['number']]
-
-    res = Response()
-    if caller['passcode'] != digits:
-        res.say('That passcode is incorrect')
-    else:
-        res.say(
-            "Passcode correct. Please wait while your message is retrieved"
-        )
-        res.pause(length="3")
-        res.say("Message follows.")
-        res.pause(length="0.5")
-        for url in caller['url']:
-            res.play(url)
-        res.pause(length="0.5")
-        res.say("End of message.")
-    res.pause(length="1")
-    res.say(
-        "Thankyou for using Lysdev's voice data storage system. "
-        "Have a nice day!"
-    )
-    return make_res(res)
-
-
-@app.route('/send_call', methods=['POST', 'GET'])
-def send_call():
-    res = ''
-    if flask.request.method == 'POST':
-        if 'delay' in flask.request.args:
-            delay = int(flask.request.args['delay'])
-            time.sleep(delay)
-
-        logging.info('sending call')
-        client.calls.create(
-            to='+61416041357',
-            from_='+61894687290',
-            url=urljoin(MY_ADDRESS, '/call')
-        )
-        logging.info('call sent!')
-        res += 'Call sent<br/>'
-
-    return res + '''
-    <form method="post"><input type="submit" value="Send call"/></form>
-    '''
 
 
 @app.route('/location/id_recieved', methods=['POST'])
@@ -212,7 +124,6 @@ def payphone_found():
 @app.route('/location', methods=['POST'])
 def location():
     res = Response()
-    # 08945004
     with res.gather(numDigits='8',
                     action=urljoin(MY_ADDRESS, '/location/id_recieved')) as g:
         g.say(
@@ -224,7 +135,6 @@ def location():
 
 @app.route('/request', methods=['POST'])
 def request_twiml():
-    # return message_system(flask.request.form.get('Caller'))
     return location()
 
 
